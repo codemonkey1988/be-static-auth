0dd2c7b46dd7dbd871a0217c434b218e4318879c
Tim Schreiner <dev@tim-schreiner.de>
2023-03-11 07:58:57 +0100

    [TASK] Update phpcsfixer

--- Tests/Unit/Service/StaticAuthenticationServiceTest.php
+++ Tests/Unit/Service/StaticAuthenticationServiceTest.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the "be_static_auth" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 namespace Codemonkey1988\BeStaticAuth\Tests\Unit\Service;
 
 use Codemonkey1988\BeStaticAuth\Service\StaticAuthenticationService;
